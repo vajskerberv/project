181cfd8b6e9af0be248842d380fe0cd9465123de
Виктор <rumbler0505@mail.ru>
2026-05-09 22:33:46 +0700

    add get_data

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 # Импорт функий маскировки номера карты и номера счёта
-from src.masks import get_mask_account, get_mask_card_number
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
@@ -19,3 +19,12 @@ def mask_account_card(account_card: str) -> str:
         masked_number = get_mask_card_number((number))
 
     return f"{name_and_number} {masked_number}"
+
+
+def get_date(ISO_format: str) -> str:
+    """
+    Функция принимает дату в формате ГГГГ-ММ-ДДТЧЧ:ММ:СС
+    и возвращает ДД-ММ-ГГГГ
+    """
+    modified_data = ISO_format[8:10] + "-" + ISO_format[5:7] + "-" + ISO_format[0:4]
+    return modified_data
