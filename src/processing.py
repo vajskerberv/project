583f520eacede2c4bfb04af0106f1e4a49fa6342
Виктор <rumbler0505@mail.ru>
2026-05-19 23:31:33 +0700

    renamed variables in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-def filter_by_state(list_with_state: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_to_sort_by_state: list, state: str = "EXECUTED") -> list:
     """
     Функция возвращает отсортированный спсиок коллекций по ключу state
     """
 
     sorted_list = []
 
-    for key in list_with_state:
+    for key in list_to_sort_by_state:
         if key.get("state") == state:
             sorted_list.append(key)
         else:
@@ -14,8 +14,8 @@ def filter_by_state(list_with_state: list, state: str = "EXECUTED") -> list:
     return sorted_list
 
 
-def sort_by_date(list_with_date: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(list_to_sort_by_date: list[dict], reverse: bool = True) -> list[dict]:
     """
     Функция возвращает список коллекций отсортированный по дате. По умолчанию - убывание
     """
-    return sorted(list_with_date, key=lambda x: x["date"], reverse=reverse)
+    return sorted(list_to_sort_by_date, key=lambda x: x["date"], reverse=reverse)
