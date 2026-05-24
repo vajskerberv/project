c0e396d7965dc87970548db54dab311826a9a150
Виктор <rumbler0505@mail.ru>
2026-05-25 01:42:40 +0700

    testing processing.py functions

---
+++ tests/test_processing.py
@@ -0,0 +1,31 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.mark.parametrize("state, expected_data_filtering",
+                         [("EXECUTED", [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                         ("CANCELED", [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
+
+def test_filter_by_state(state, expected_data_filtering):
+    result = filter_by_state(expected_data_filtering, state)
+    expected = expected_data_filtering
+    assert result == expected
+
+
+@pytest.mark.parametrize("sort_sequence, expected_data_sorting",
+                         [(True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                         (False, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])])
+
+def test_sort_by_date(expected_data_sorting, sort_sequence):
+    result = sort_by_date(expected_data_sorting)
+    expected = expected_data_sorting
+    assert result == expected
